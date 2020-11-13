10cbe7865af8d8ca67d35493ab841147ee5370f2
bernat <elbernat@gmail.com>
2020-11-13 15:42:54 -0500

    new project with firebase

---
+++ project-3/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <HelloWorld/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300&display=swap");
+html {
+  background: #08070f;
+}
+
+.done {
+  text-decoration: line-through;
+}
+
+.todo {
+  cursor : pointer;
+}
+
+#app {
+  font-family: "IBM Plex Sans", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: greenyellow;
+  margin-top: 60px;
+}
+</style>
---
+++ project-3/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{ state.msg }}
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+export default {
+    setup() {
+    const state = reactive({
+      msg : 'Hello World',
+    });
+
+    return {
+      state,
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
